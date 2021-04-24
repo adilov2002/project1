011263ec3a8f99114ff85e36a449f00834aaa152
ZhanniyetAd <joni.2002.08@gmail.com>
2021-04-24 17:00:48 +0600

    First Commit

--- src/main/java/practice/springboot/demo/controllers/HomeController.java
+++ src/main/java/practice/springboot/demo/controllers/HomeController.java
@@ -3,6 +3,9 @@ package practice.springboot.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import practice.springboot.demo.db.Cars;
 import practice.springboot.demo.db.DBManager;
 
@@ -23,4 +26,25 @@ public class HomeController {
     public String about(){
         return "about";
     }
+
+    @GetMapping(value = "/addcar")
+    public String addCar(){
+        return "addcar";
+    }
+
+    @PostMapping(value = "/addcar")
+    public String toAddCar(@RequestParam(name = "name") String name,
+                           @RequestParam(name = "model") String model,
+                           @RequestParam(name = "speed") int speed){
+        DBManager.addCar(new Cars(null, name, model, speed));
+        return "redirect:/addcar?success";
+    }
+
+    @GetMapping(value = "/details/{carId}")
+    public String details(@PathVariable(name = "carId") Long id, Model model){
+        Cars car = DBManager.getCar(id);
+        model.addAttribute("car", car);
+        return "details";
+    }
+
 }
--- src/main/java/practice/springboot/demo/db/DBManager.java
+++ src/main/java/practice/springboot/demo/db/DBManager.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class DBManager {
 
-    private static ArrayList<Cars> cars;
+    private static ArrayList<Cars> cars = new ArrayList<>();
 
     static {
         cars.add(new Cars(1L,"Honda", "H1", 220));
@@ -13,7 +13,25 @@ public class DBManager {
         cars.add(new Cars(4L,"Tayota", "T3", 260));
     }
 
+    private static Long id = 5L;
     public static ArrayList<Cars> getAllCars(){
         return cars;
     }
+
+    public static void addCar(Cars car){
+        car.setId(id);
+        cars.add(car);
+        id++;
+    }
+
+    public static Cars getCar(Long id){
+        for(Cars c : cars){
+            if (c.getId() == id){
+                return c;
+            }
+        }
+        return null;
+    }
+
+
 }
